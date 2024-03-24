311d80057159ad54b49acfcfb13a546ad31a7b0e
Ian-Fund <ianfund117@gmail.com>
2024-03-23 19:34:27 -0500

    Add image and search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../src/media/FU_logo.png";
+import tempImage from "../src/media/Lilly_Bench.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Runesite</h1>
       </header>
+      <div className="search-bar">
+        <input type="text" placeholder="Search for a rune..." />
+      </div>
+      <div className="Content">
+        <div className="Description-Section">
+          <div className="Image-Description">
+            <img src={tempImage} className="Content-image" alt="content" />
+            <p className="Content-description">This is a description.</p>
+          </div>
+          <button className="Content-button">Click me</button>
+        </div>
+      </div>
     </div>
   );
 }
